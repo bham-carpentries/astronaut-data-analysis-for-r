197583d27c547acc736724d46b6b4d76e540ea00
kkh451 <kamillaharding@gmail.com>
2025-03-10 20:08:26 +0200

    Add comments and print statements

--- eva_data_analysis.R
+++ eva_data_analysis.R
@@ -6,10 +6,15 @@ library(lubridate)
 library(readr)
 library(stringr)
 
+# https://data.nasa.gov/resource/eva.json (with modifications)
 input_file <- "eva-data.json"
 output_file <- "eva-data.csv"
 graph_file <- "cumulative_eva_graph.png"
 
+print("--START--")
+print("Reading JSON file")
+
+# Read the data from a JSON file into a Pandas dataframe
 eva_data <- fromJSON(input_file, flatten = TRUE) %>%
  mutate(eva = as.numeric(eva)) %>%
  mutate(date = ymd_hms(date)) %>%
@@ -17,14 +22,19 @@ eva_data <- fromJSON(input_file, flatten = TRUE) %>%
  arrange(date) %>%
  mutate(year = year(date))
 
+print("Saving to CSV file")
+# Save dataframe to CSV file for later analysis
 write_csv(eva_data, output_file)
 
+print("Plotting cumulative spacewalk duration and saving to file")
+# Plot cumulative time spent in space over years
 time_in_space_plot <- eva_data %>%
   rowwise() %>%
   mutate(duration_hours =
                   sum(as.numeric(str_split_1(duration, "\\:"))/c(1, 60))
   ) %>%
   ungroup() %>%
+  # Calculate cumulative time
   mutate(cumulative_time = cumsum(duration_hours)) %>%
   ggplot(aes(x = year, y = cumulative_time)) +
   geom_line(color = "black") +
@@ -35,3 +45,4 @@ time_in_space_plot <- eva_data %>%
   theme_minimal()
 
 ggsave(graph_file, width = 8, height = 6, plot = time_in_space_plot)
+print("--END--")
