4ea8fa31e445f4fce08f086c1beeff84f9abf7e2
kkh451 <kamillaharding@gmail.com>
2025-03-10 17:20:34 +0200

    Update variable names

--- eva_data_analysis.R
+++ eva_data_analysis.R
@@ -3,14 +3,14 @@ library(ggplot2)
 
 # https://data.nasa.gov/resource/eva.json (with modifications)
 # File paths
-data_f <- "eva-data.json"
-data_t <- "eva-data.csv"
-g_file <- "cumulative_eva_graph.png"
+input_file <- "eva-data.json"
+output_file <- "eva-data.csv"
+graph_file <- "cumulative_eva_graph.png"
 
 fieldnames <- c("eva", "country", "crew", "vehicle", "date", "duration", "purpose")
 
 data <- list()
-data_raw <- readLines(data_f, warn = FALSE)
+data_raw <- readLines(input_file, warn = FALSE)
 
 # 374
 for (i in 1:374) {
@@ -29,15 +29,15 @@ w <- 0
 for (i in data) {  # Iterate manually
 
   if ("duration" %in% names(data[[j]])) {
-    tt <- data[[j]]$duration
+    time_text <- data[[j]]$duration
 
-    if (tt == "") {
+    if (time_text == "") {
       # Do nothing if empty
     } else {
-      t_parts <- strsplit(tt, ":")[[1]]
-      ttt <- as.numeric(t_parts[1]) + as.numeric(t_parts[2]) / 60  # Convert to hours
-      print(ttt)
-      time <- c(time, ttt)
+      t_parts <- strsplit(time_text, ":")[[1]]
+      t_hours <- as.numeric(t_parts[1]) + as.numeric(t_parts[2]) / 60  # Convert to hours
+      print(t_hours)
+      time <- c(time, t_hours)
 
       if (("date" %in% names(data[[j]]) & ("eva" %in% names(data[[j]])))) {
         date <- as.Date(substr(data[[j]]$date, 1, 10), format = "%Y-%m-%d")
@@ -55,9 +55,9 @@ for (i in data) {  # Iterate manually
   if (exists("row_data")) {
     print(row_data)
     if (w==0) {
-      write.table(row_data, data_t, sep = ",", row.names = FALSE, col.names = TRUE, append = FALSE)
+      write.table(row_data, output_file, sep = ",", row.names = FALSE, col.names = TRUE, append = FALSE)
     } else {
-      write.table(row_data, data_t, sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
+      write.table(row_data, output_file, sep = ",", row.names = FALSE, col.names = FALSE, append = TRUE)
     }
     w <- w+1
     rm(row_data)
@@ -66,10 +66,10 @@ for (i in data) {  # Iterate manually
   j <- j + 1
 }
 
-if (!exists("ct")) {ct <- c(0)}
+if (!exists("cumulative_time")) {cumulative_time <- c(0)}
 
 for (k in time) {
-  ct <- c(ct, ct[length(ct)] + k)
+  cumulative_time <- c(cumulative_time, cumulative_time[length(cumulative_time)] + k)
 }
 
 sorted_indices <- order(dates)
@@ -77,21 +77,21 @@ years <- years[sorted_indices]
 time <- time[sorted_indices]
 
 # Print total time in space
-print(ct[length(ct)])
+print(cumulative_time[length(cumulative_time)])
 
 tdf <- data.frame(
   years = years,
-  ct = ct[-1]
+  cumulative_time = cumulative_time[-1]
 )
 
 
 # Plot the data
-ggplot(tdf, aes(x = years, y = ct)) + geom_line(color = "black") + geom_point(color = "black") +
+ggplot(tdf, aes(x = years, y = cumulative_time)) + geom_line(color = "black") + geom_point(color = "black") +
   labs( x = "Year", y = "Total time spent in space to date (hours)", title = "Cumulative Spacewalk Time" ) + theme_minimal()
 
 # Correction for repeatability
-ct <- c(0)
+cumulative_time <- c(0)
 
 # Save plot
-ggsave(g_file, width = 8, height = 6)
+ggsave(graph_file, width = 8, height = 6)
 
